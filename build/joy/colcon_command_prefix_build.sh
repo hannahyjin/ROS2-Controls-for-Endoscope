6719fea4b7526fbafdeef163f8c510e874d60a4f
akannan05 <akannan05@g.ucla.edu>
2025-04-23 18:59:50 -0700

    Remove build artifacts

--- build/joy/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/bmes-endoscope-25/joystick_ws/install/sdl2_vendor/share/sdl2_vendor/package.sh"
--- build/wiimote/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/bmes-endoscope-25/joystick_ws/install/wiimote_msgs/share/wiimote_msgs/package.sh"
